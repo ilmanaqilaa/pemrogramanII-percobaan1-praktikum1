d6b422ae0f111d5622328cd40f51f51971155bda
ilmanaqilaa <ilmanaqilaa2@gmail.com>
2021-11-05 21:23:03 +0700

    push pertama

---
+++ P1_1_1204055/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_1_1204055
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Halo Halo From Visual Studio, I'm Muhammad Ilman Aqilaa");
+        }
+    }
+}
